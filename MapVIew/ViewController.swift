c2b1f52d6f68d53809f5255998132a73405b3a6c
kwackjihoon <chunjoo4@naver.com>
2018-06-07 12:47:07 +0900

    Add MapView

--- MapVIew/ViewController.swift
+++ MapVIew/ViewController.swift
@@ -5,14 +5,32 @@
 //  Created by dit on 2018. 6. 7..
 //  Copyright © 2018년 dit. All rights reserved.
 //
-
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapVIew: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //위도 경도 설정
+        let loc : CLLocationCoordinate2D = CLLocationCoordinate2D (latitude: 35.165999, longitude: 129.072543)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
+        
+        let region = MKCoordinateRegion(center: loc, span: span)
+        
+        mapVIew.setRegion(region, animated: true)
+        
+        let anno = MKPointAnnotation()
+        anno.coordinate = loc
+        anno.title = "DIT 동의과학대학교"
+        anno.subtitle = "내꿈이 실현되는곳"
+        mapVIew.addAnnotation(anno)
     }
 
     override func didReceiveMemoryWarning() {
